9de58cfe56501ad5bfd0aafa9a54f098e60e6d86
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2023-09-21 18:38:03 +0200

    Static directory

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import path from 'node:path';
+import url from 'node:url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const router = express.Router();
 
 router.use('/', await (await import('./routers/example.js')).default);
+
+router.use('/static', express.static(path.join(__dirname, '../static')));
 
 export default router;
